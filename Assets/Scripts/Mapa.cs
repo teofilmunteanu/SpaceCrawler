96433d5253b03131a21586c33db501f8e0b609aa
Munteanu Teofil <andreiteofil01@gmail.com>
2022-05-06 01:10:38 +0300

    Better heights test

--- Assets/Scripts/Mapa.cs
+++ Assets/Scripts/Mapa.cs
@@ -8,6 +8,11 @@ public class Mapa : MonoBehaviour
     {
         public float x, z;
         public float height;
+
+        public Vector3 position()
+        {
+            return new Vector3(x, 0 ,z);
+        }
     }
 
     public GameObject block;
@@ -15,34 +20,33 @@ public class Mapa : MonoBehaviour
     GameObject[,] o = new GameObject[100, 100];
 
     float baseScale = 2;
+    int lines = 50, columns = 50;
+
     void Start()
     {
-        //inaltimi initiale - pentru test
-        for(int i = 0;i<10;i++)
+        for (var i = 0; i < lines; i++)
         {
-            for (var j = 0; j < 10; j++)
-            {
-                m[i,j].height = 4;
-            }
-        }    
-
-        //test
-        m[2,3].height = 8;
-
-        for (var i = 0; i < 10; i++)
-        {
-            for (var j = 0; j < 10; j++)
+            for (var j = 0; j < columns; j++)
             {
                 o[i,j] = Instantiate(block, new Vector3(i * baseScale, 0, j * baseScale), Quaternion.identity);
                 //store position on the grid
-                m[i,j].x = i * 2;
-                m[i,j].z = j * 2;
-
-                //store height
-                o[i,j].transform.localScale = new Vector3(baseScale, m[i,j].height, baseScale);
-                o[i,j].transform.position = o[i,j].transform.position + new Vector3(0, m[i,j].height / 2, 0);
+                m[i,j].x = i * baseScale;
+                m[i,j].z = j * baseScale;     
             }
         }
+
+
+        //inaltimi initiale - pentru test
+        for(int i = 0;i<lines;i++)
+        {
+            for (var j = 0; j < columns; j++)
+            {
+                //store height
+                m[i,j].height = 3 + Random.Range(Vector3.Distance(m[0,0].position(), m[i,j/2].position())/5 - 2f, Vector3.Distance(m[0,0].position(), m[i,j/2].position())/5 + 2f);
+                o[i,j].transform.position = o[i,j].transform.position + new Vector3(0, m[i,j].height / 2, 0);
+                o[i,j].transform.localScale = new Vector3(baseScale, m[i,j].height, baseScale);
+            }
+        }    
     }
 
     // Update is called once per frame
